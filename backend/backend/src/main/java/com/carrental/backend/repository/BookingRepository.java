24c3c2745dd9d3ae9bf509e121726583619ce5c9
Siddartha-balla <23211a6712@bvrit.ac.in>
2026-01-19 23:47:41 +0530

    Delete backend/backend directory

--- backend/backend/src/main/java/com/carrental/backend/repository/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.carrental.backend.repository;
-
-import java.util.List;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.carrental.backend.model.Booking;
-
-public interface BookingRepository extends MongoRepository<Booking, String> {
-    List<Booking> findByUserEmail(String userEmail);
-}
--- backend/backend/src/main/java/com/carrental/backend/repository/CarRepository.java
+++
@@ -1,7 +0,0 @@
-package com.carrental.backend.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.carrental.backend.model.Car;
-
-public interface CarRepository extends MongoRepository<Car, String> {
-}
